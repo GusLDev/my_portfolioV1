6c7371aba0c8bb54613c4abd627d9a6d19cf3327
GusLDev <gustavoml2097@hotmail.com>
2023-07-29 16:54:01 -0600

    i added some links to my page

--- src/app/components/Home.jsx
+++ src/app/components/Home.jsx
@@ -7,18 +7,20 @@ const Home = () => {
       <div className={Styles.info}>
         <div className={Styles.fcolumn}>
           <h2>Hello, My Name Is<br />Gustavo.</h2>
-          <h3>Systems engineer <span className={Styles.university}>ITT</span></h3>
+          <h3>Systems engineer <a className={Styles.university} href='https://www.tepic.tecnm.mx/'>ITT</a></h3>
         </div>
         <div className={Styles.scolumn}>
-          <Image className={Styles.profileImage}
-            src="/assets/gml.png"
-            width={100}
-            height={100}
-            alt="Picture of the author"
-          />
+          <a href='https://www.linkedin.com/in/gustavo-lemus-637b38258/'>
+            <Image className={Styles.profileImage}
+              src="/assets/gml.png"
+              width={100}
+              height={100}
+              alt="Picture of the author"
+            />
+          </a>
         </div>
         <div className={Styles.tcolumn}>
-          <h2 className={Styles.position}>FRONT-END <br/><span className={Styles.dev}>DEVELOPER</span></h2>
+          <h2 className={Styles.position}>FRONT-END <br /><span className={Styles.dev}>DEVELOPER</span></h2>
           <h3>I like to design, create and <br /> learn new things.</h3>
         </div>
       </div>
--- src/app/components/Work.jsx
+++ src/app/components/Work.jsx
@@ -4,7 +4,18 @@ import Styles from '../styles/Work.module.css'
 
 const Work = () => {
   return (
-    <div className={Styles.container}>Work</div>
+    <div className={Styles.container}>
+      <div className={Styles.header}>
+        < div className={Styles.titles}>
+          <h2 className={Styles.latest}>My Latest Works</h2>
+          <h3 className={Styles.look}>Take a look!</h3>
+        </div>
+          <a className={Styles.git} href='https://github.com/GusLDev'>EXPLORE MY PROFILE</a>
+      </div>
+      <div className={Styles.works}>
+
+      </div>
+    </div>
   )
 }
 
