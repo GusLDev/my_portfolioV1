ec0f94dc5f72b4444fe90eb6b80b74e2477e3876
GusLDev <gustavoml2097@hotmail.com>
2023-07-30 12:57:38 -0600

    I added some features to home module

--- src/app/components/Work.jsx
+++ src/app/components/Work.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Styles from '../styles/Work.module.css'
-
+import Image from 'next/image'
 
 const Work = () => {
   return (
@@ -10,10 +10,26 @@ const Work = () => {
           <h2 className={Styles.latest}>My Latest Works</h2>
           <h3 className={Styles.look}>Take a look!</h3>
         </div>
-          <a className={Styles.git} href='https://github.com/GusLDev'>EXPLORE MY PROFILE</a>
+        <a className={Styles.git} href='https://github.com/GusLDev'>EXPLORE MY PROFILE</a>
       </div>
       <div className={Styles.works}>
-
+        <a href="https://www.naayaritours.com/">
+        <figure className={Styles.card}>
+            <Image className={Styles.workImage}
+              src="/assets/NaayariTours.png"
+              width={500}
+              height={500}
+              alt="Picture of the author"
+            />
+            <div className={Styles.cap}>
+              <h3 className={Styles.title}>Naayari Tours Web Site</h3>
+              <p className={Styles.desc}>
+                We create a web site for Naayari Tours dedicated to
+                reservations for their clients.
+              </p>
+            </div>
+          </figure>
+        </a>
       </div>
     </div>
   )
