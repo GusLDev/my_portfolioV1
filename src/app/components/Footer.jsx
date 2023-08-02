71755bf007bda90094c5515c7558a2ae5fc86987
GusLDev <gustavoml2097@hotmail.com>
2023-08-01 18:25:26 -0700

    Footer Added

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -3,7 +3,8 @@ import Styles from '../styles/Footer.module.css'
 const Footer = () => {
   return (
     <div className={Styles.container}>
-
+        <h2 className={Styles.copy}>Copyright © By Gustavo Marin Lemus | All Rights Reserved</h2>
+        <h3 className={Styles.sapere}>Immanuel kant - "I dare you to think"</h3>
     </div>
   )
 }
